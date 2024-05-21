be7fc44ae9af73d6db6b0a71301a5e859117d4fd
COMPLING96470128 <3452609956@qq.com>
2024-05-21 13:05:04 +0800

    Create main.mm

---
+++ main.mm
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#import <Foundation/Foundation.h>
+@interface
+
+@end
+
+int main(void)
+{
+  @autoreleasepool{
+    NSLog(@"328746827");
+  }
+  printf("342");
+  return 0;
+}
